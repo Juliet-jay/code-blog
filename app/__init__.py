245a7b1d220fb553ed4487f729d1c0e0a51a24b5
kathure <juliekmuthee@gmail.com>
2019-08-10 12:52:34 +0300

    simple mde configuration

--- app/__init__.py
+++ app/__init__.py
@@ -11,3 +11,6 @@ def create_app(config_name):
     '''
 # Initialising application
 app = Flask(__name__)
+
+# Creating the app configurations
+app.config.from_object(config_options[config_name])
---
+++ config.py
@@ -0,0 +1,54 @@
+import os
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 588
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+class ProdConfig(Config):
+    '''
+    Pruduction configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL")
+    pass
+
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings 
+    '''
+    
+    pass
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+    
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://juliet:1234@localhost/bloggy'
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
+}
