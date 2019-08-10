4cb4a2c38645cf41c6b4c14395c73b6f589ab80b
kathure <juliekmuthee@gmail.com>
2019-08-10 14:58:21 +0300

    created login in forms

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 
 def create_app(config_name):
@@ -13,4 +18,3 @@ def create_app(config_name):
 app = Flask(__name__)
 
 # Creating the app configurations
-app.config.from_object(config_options[config_name])
