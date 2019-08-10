b1b01fe68eea1c35de728d31bebf562be1ccfdbe
kathure <juliekmuthee@gmail.com>
2019-08-10 14:07:44 +0300

    complete folder stucture

--- manage.py
+++ manage.py
@@ -13,3 +13,24 @@ app = create_app('production')
 
 # Create manager instance 
 manager = Manager(app)
+
+manager.add_command('server',Server)
+manager.add_command('db',MigrateCommand)
+
+@manager.command
+def test():
+    '''
+    Run the unit tests
+    '''
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict( app=app, db=db, User=User, Review=Review, Role=Role)
+
+if __name__ == '__main__':
+    #     app.secret_key = 'qwerty12345' 
+    manager.run()
+
